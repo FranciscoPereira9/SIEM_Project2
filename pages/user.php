7b70b7eec2ac42ccc6c6013a9203d0c6bf5293ac
Simão Quintans <up201504961@fe.up.pt>
2020-12-29 17:05:28 +0000

    x

--- pages/user.php
+++ pages/user.php
@@ -10,35 +10,44 @@
 	 			echo $_SESSION['signinSuccess'];
 				$_SESSION['signinSuccess'] = NULL;
 			}
-		?>
-
-		<b>LOGIN:</b><br><br>
-		<form method="POST" action="../actions/actionLogIn.php">
-		   Login: 	<input type="text" name="email_login"></input><br>
-		Password: 	<input type="password" name="password_login"></input><br>
-					<input type="submit" name="login" value="LOGIN"></input><br>
-		</form>
 		
-		<b>SIGN IN:</b><br><br>
-		<?php 
-			if(isset($_SESSION['signinIncomplete'])){
-	 			echo $_SESSION['signinIncomplete'];
-				$_SESSION['signinIncomplete']=NULL;
+			if($_SESSION['username']){
+				?>
+				<a href="../actions/actionLogout.php"> Logout </a>
+				<?php
 			}
-			if(isset($_SESSION['signinUserFail'])){
-	 			echo $_SESSION['signinUserFail'];
-				$_SESSION['signinUserFail']=NULL;
+			else{
+				
+				echo "<b>LOGIN:</b><br><br>";
+				echo "<form method=\"POST\" action=\"../actions/actionLogIn.php\">";
+				   echo"Login: 	<input type=\"text\" name=\"email_login\"></input><br>";
+				echo"Password: 	<input type=\"password\" name=\"password_login\"></input><br>";
+							echo"<input type=\"submit\" name=\"login\" value=\"LOGIN\"></input><br>";
+				echo"</form>";
+				
+				echo"<b>SIGN IN:</b><br><br>";
+				
+					if(isset($_SESSION['signinIncomplete'])){
+						echo $_SESSION['signinIncomplete'];
+						$_SESSION['signinIncomplete']=NULL;
+					}
+					if(isset($_SESSION['signinUserFail'])){
+						echo $_SESSION['signinUserFail'];
+						$_SESSION['signinUserFail']=NULL;
+					}
+				
+
+				echo "<form method=\"POST\" action=\"../actions/actionCreateUser.php\">";
+				
+				echo"Firstname: 	<input type=\"text\" name=\"firstname\"></input><br>";
+				echo"Lastname: 	<input type=\"text\" name=\"lastname\"></input><br>";
+				   echo"Login: 	<input type=\"text\" name=\"email\"></input><br>";
+				echo"Password: 	<input type=\"password\" name=\"password\"></input><br>";
+							echo"<input type=\"submit\" name=\"register\" value=\"REGISTER\"></input><br>";
+				echo"</form>";
+				
 			}
 		?>
-
-		<form method="POST" action="../actions/actionCreateUser.php">
-		
-		Firstname: 	<input type="text" name="firstname"></input><br>
-		Lastname: 	<input type="text" name="lastname"></input><br>
-		   Login: 	<input type="text" name="email"></input><br>
-		Password: 	<input type="password" name="password"></input><br>
-					<input type="submit" name="register" value="REGISTER"></input><br>
-		</form>
 	</body>
 </html>
 	
